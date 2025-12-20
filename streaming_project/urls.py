65d0c95dfc5926fbd9c40d5f8b992709059ad016
rodolfobredoff <rodolfobredoff@gmail.com>
2025-12-20 11:29:14 -0300

    Correção final de Gunicorn e URLs

--- streaming_project/urls.py
+++ streaming_project/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')), # Isso conecta suas views (Home, Upload)
-]
+    path('', include('core.urls')), # Conecta o app de vídeos
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
