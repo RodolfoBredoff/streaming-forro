1b8977155e36811797af36aa9f85be33ac4d8da8
rodolfobredoff <rodolfobredoff@gmail.com>
2025-12-26 16:18:34 -0300

    Fix: inserindo botôes de busca e left-right

--- core/views.py
+++ core/views.py
@@ -20,11 +20,39 @@ class CourseDetailView(LoginRequiredMixin, DetailView):
     template_name = 'core/course_detail.html'
     context_object_name = 'course'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('search_lesson')
+        
+        if query:
+            # Filtra aulas do curso que contenham o nome pesquisado
+            context['search_results'] = Lesson.objects.filter(
+                module__course=self.object, 
+                title__icontains=query
+            )
+            context['is_searching'] = True
+        return context
+
 # Player do vídeo antigo (Mantenha se quiser usar o player individual)
 class VideoDetailView(LoginRequiredMixin, DetailView):
     model = Video
     template_name = 'core/video_detail.html'
     context_object_name = 'video'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        video_atual = self.object
+        
+        # Tentamos encontrar a aula que contém este vídeo
+        aula = video_atual.lesson_set.first() 
+        
+        if aula:
+            modulo = aula.module
+            # Busca a próxima aula e a anterior no mesmo módulo
+            context['proxima_aula'] = modulo.lessons.filter(id__gt=aula.id).order_by('id').first()
+            context['aula_anterior'] = modulo.lessons.filter(id__lt=aula.id).order_by('-id').first()
+        
+        return context
 
 # Upload de Vídeo bruto
 class VideoCreateView(LoginRequiredMixin, CreateView):
