5dc75761f9c1f97d76b7871d04b4d786b4d153f4
rodolfobredoff <rodolfobredoff@gmail.com>
2025-12-26 11:56:25 -0300

    Fix: Ajuste de csrf para upload de video

--- streaming_project/settings.py
+++ streaming_project/settings.py
@@ -26,11 +26,15 @@ INSTALLED_APPS = [
     'core',     # Seu app
 ]
 
+# settings.py
+CSRF_USE_SESSIONS = True
+CSRF_SESSION_AUTH_INFO = True  # Para versões recentes do Django
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
