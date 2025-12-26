6cb59be295a8e215b5781ab25ad34b8bc02db17e
rodolfobredoff <rodolfobredoff@gmail.com>
2025-12-26 14:29:58 -0300

    Fix: adicionando pk nas rotas das views

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CourseListView, VideoDetailView # e outras que usar
+from .views import CourseListView, CourseDetailView
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
-    path('curso/<int:pk>/', CourseDetailView.as_view(), name='course_detail'), # Mudamos para int:pk
+    path('curso/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
+] 
