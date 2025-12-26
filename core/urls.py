cb10726a3ee96be8c0ceed541a31f62444eb276f
rodolfobredoff <rodolfobredoff@gmail.com>
2025-12-26 14:39:44 -0300

    Fix: estilizando modulos

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CourseListView, CourseDetailView
-
+from .views import CourseListView, CourseDetailView, VideoDetailView
 urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
     path('curso/<int:pk>/', CourseDetailView.as_view(), name='course_detail'),
