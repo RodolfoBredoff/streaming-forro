d566f8e8f34306f502e87809e0c786bac2ae1801
rodolfobredoff <rodolfobredoff@gmail.com>
2025-12-26 14:24:23 -0300

    Fix: ajuste de rotas das views

--- core/urls.py
+++ core/urls.py
@@ -3,5 +3,4 @@ from .views import CourseListView, VideoDetailView # e outras que usar
 
 urlpatterns = [
     path('', CourseListView.as_view(), name='course_list'),
-    # path('video/<int:pk>/', VideoDetailView.as_view(), name='video_detail'),
-]
+    path('curso/<slug:slug>/', CourseDetailView.as_view(), name='course_detail'), ]# Nova rota
